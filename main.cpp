a9669c0cd40229020edb1c41b5009cf17443ddb0
YazhenHan <yazhenhan@foxmail.com>
2021-08-18 08:00:27 +0800

    libigl

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
-#include "glfwwindow.hpp"
+//#include "glfwwindow.hpp"
+//
+//int main()
+//{
+//    GlfwWindow glfwWindow;
+//    Shader modelLoadingShader("universal.vs", "face.fs");
+//    Shader pointShader("universal.vs", "point.fs");
+//    Shader lineShader("universal.vs", "line.fs");
+//    Model ourModel;
+//    glfwWindow.draw(ourModel, modelLoadingShader, pointShader, lineShader);
+//    glfwWindow.cleanUp();
+//    return 0;
+//}
+
+#include <igl/readOBJ.h>
+#include <igl/opengl/glfw/Viewer.h>
+
+Eigen::MatrixXd V;
+Eigen::MatrixXi F;
 
 int main()
 {
-    GlfwWindow glfwWindow;
-    Shader modelLoadingShader("universal.vs", "face.fs");
-    Shader pointShader("universal.vs", "point.fs");
-    Shader lineShader("universal.vs", "line.fs");
-    Model ourModel;
-    glfwWindow.draw(ourModel, modelLoadingShader, pointShader, lineShader);
-    glfwWindow.cleanUp();
+    igl::readOBJ("assets/diablo3_pose.obj", V, F);
+    igl::opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(V, F);
+    viewer.launch();
     return 0;
 }
